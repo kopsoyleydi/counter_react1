83703a5f0f5ed09b907a340cb3f06e4f031bd52a
Bexultan Kuralbay <30636@iitu.edu.kz>
2023-01-20 01:44:16 +0600

    counter1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
+/* eslint-disable react/no-direct-mutation-state */
+/* eslint-disable no-useless-constructor */
+/* eslint-disable no-undef */
+// eslint-disable-next-line no-unused-vars
+import { Component } from "react";
 import './App.css';
+// eslint-disable-next-line no-unused-vars
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function randomNumber(min, max){
+  return Math.floor(Math.random() * (max - min) + min);
+}
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      curValue: 10
+    };
+  }
+
+  handleClickPlus = () =>{
+    this.setState({curValue: ++this.state.curValue })
+  }
+  handleClickMinus = () =>{
+    this.setState({curValue: --this.state.curValue })
+  }
+  handleClickRes = () => {
+    this.setState({curValue: 0})
+  }
+  handleClickRandom = () =>{
+    this.setState({curValue : randomNumber(-50,50)})
+  }
+//function getRandomArbitrary(min, max) {
+  //return Math.random() * (max - min) + min;
+//} 
+  render(props){
+    return(
+      <div className="app">
+          <div class="counter">{this.state.curValue}</div>
+          <div class="controls">
+          <button onClick =  {this.handleClickPlus}>INC</button>
+          <button onClick={this.handleClickMinus}>DEC</button>
+          <button onClick={this.handleClickRandom}>RND</button>
+          <button onClick={this.handleClickRes}>RESET</button>
+          </div>
+        </div>
+    )
+    }
 }
 
 export default App;
+
